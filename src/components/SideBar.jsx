b06cafcd8e0ad8e6bc1546a0e84550e056d5c158
Talha <talhaiftikhar12549@gmail.com>
2025-01-16 17:29:33 +0500

    resolved error in sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,42 +11,42 @@ import reviewsEllipse from "../assets/ReviewsEllipse.png"
 export default function SideBar() {
     return (
         <>
-            <section className=" fixed flex flex-row">
+            <section className=" fixed flex flex-row justify-evenly top-0 bottom-0 -z-50">
 
                 {/* left Space Bar */}
-                <div className="bg-[#F9F9FB] h-[100vh] w-[10px]">
+                <div className="bg-[#F9F9FB] w-[10px]">
                 </div>
                 {/* left Space Bar */}
 
                 {/* SideBar */}
-                <div className="bg-[#F9F9FB] w-[106px] h-[100vh] flex flex-col items-center">
+                <div className="bg-[#F9F9FB] w-[106px] h-[100%] flex flex-col items-center justify-between py-[50%]">
                     {/* Nav Items */}
-                    <div className=" w-[100%] flex-none">
+                    <div className=" w-[100%] ">
 
-                        <div className="flex flex-col items-center cursor-pointer py-4  ">
+                        <div className="flex flex-col items-center cursor-pointer py-3  ">
                             <img src={dashBoardIcon} alt="dashBoardIcon" className="py-2" />
                             <p className="font-montserrat text-xs font-medium text-[#8F95A9]">DASHBOARD</p>
                         </div>
 
-                        <div className="flex flex-col items-center cursor-pointer py-4">
+                        <div className="flex flex-col items-center cursor-pointer py-3 relative">
                             <img src={notificationEllipse} alt="notificationEllipse" className="absolute left-[55%]" />
                             <img src={inboxIcon} alt="inboxIcon" className="py-2" />
                             <p className="font-montserrat text-xs font-medium text-[#8F95A9]">INBOX</p>
                         </div>
 
-                        <div className="flex flex-col items-center cursor-pointer py-4">
+                        <div className="flex flex-col items-center cursor-pointer py-3">
                             <img src={scheduleIcon} alt="scheduleIcon" className="py-2" />
                             <p className="font-montserrat text-xs font-medium text-[#8F95A9]">SCHEDULE</p>
                         </div>
 
-                        <div className="flex flex-col items-center cursor-pointer py-4 relative">
+                        <div className="flex flex-col items-center cursor-pointer py-3 relative">
                             <img src={reviewsEllipse} alt="reviewsEllipse" className="absolute left-[55%]" />
                             <img src={reviewIcon} alt="reviewIcon" className="py-2" />
                             <p className="font-montserrat text-xs font-medium text-[#8F95A9]">REVIEWS</p>
                             <img src={curveRectangleUp} alt="curveRectangleDown" className="absolute bottom-0 right-0" />
                         </div>
 
-                        <div className="flex flex-col items-center cursor-pointer bg-[#ffffff] py-4 rounded-l-2xl">
+                        <div className="flex flex-col items-center cursor-pointer bg-[#ffffff] py-3 rounded-l-2xl">
                             <img src={settingsIcon} alt="settingsIcon" className="py-2" />
                             <p className="font-montserrat text-xs font-medium text-[#8F95A9]">SETTINGS</p>
                         </div>
@@ -55,8 +55,8 @@ export default function SideBar() {
                     {/* Nav Items */}
 
                     {/* Log Out  */}
-                    <div className=" w-[100%] relative flex flex-col justify-around h-[100%] ">
-                        <div  >
+                    <div className=" w-[100%] flex flex-col justify-between ">
+                        <div  className="relative">
                             <img src={curveRectangleDown} alt="curveRectangleDown" className="absolute top-0 right-0 "/>
                         </div>
 
