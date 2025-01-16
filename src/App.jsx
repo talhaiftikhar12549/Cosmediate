dbf293d9bc653618c7734fbeb26851a0cefaa1aa
Talha <talhaiftikhar12549@gmail.com>
2025-01-16 16:57:38 +0500

    sidebar compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from "./components/NavBar"
+import SideBar from './components/SideBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <NavBar/>
+      <SideBar/>
     </>
   )
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,76 @@
+import logOut from "../assets/SignOut.png"
+import dashBoardIcon from "../assets/DashBoard.png"
+import inboxIcon from "../assets/InboxIcon.png"
+import scheduleIcon from "../assets/ScheduleIcon.png"
+import reviewIcon from "../assets/ReviewIcon.png"
+import settingsIcon from "../assets/SettingsIcon.png"
+import curveRectangleUp from "../assets/CurveRectangleUp.png"
+import curveRectangleDown from "../assets/CurveRectangleDown.png"
+import notificationEllipse from "../assets/NotificationEllipse.png"
+import reviewsEllipse from "../assets/ReviewsEllipse.png"
+export default function SideBar() {
+    return (
+        <>
+            <section className=" fixed flex flex-row">
+
+                {/* left Space Bar */}
+                <div className="bg-[#F9F9FB] h-[100vh] w-[10px]">
+                </div>
+                {/* left Space Bar */}
+
+                {/* SideBar */}
+                <div className="bg-[#F9F9FB] w-[106px] h-[100vh] flex flex-col items-center">
+                    {/* Nav Items */}
+                    <div className=" w-[100%] flex-none">
+
+                        <div className="flex flex-col items-center cursor-pointer py-4  ">
+                            <img src={dashBoardIcon} alt="dashBoardIcon" className="py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">DASHBOARD</p>
+                        </div>
+
+                        <div className="flex flex-col items-center cursor-pointer py-4">
+                            <img src={notificationEllipse} alt="notificationEllipse" className="absolute left-[55%]" />
+                            <img src={inboxIcon} alt="inboxIcon" className="py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">INBOX</p>
+                        </div>
+
+                        <div className="flex flex-col items-center cursor-pointer py-4">
+                            <img src={scheduleIcon} alt="scheduleIcon" className="py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">SCHEDULE</p>
+                        </div>
+
+                        <div className="flex flex-col items-center cursor-pointer py-4 relative">
+                            <img src={reviewsEllipse} alt="reviewsEllipse" className="absolute left-[55%]" />
+                            <img src={reviewIcon} alt="reviewIcon" className="py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">REVIEWS</p>
+                            <img src={curveRectangleUp} alt="curveRectangleDown" className="absolute bottom-0 right-0" />
+                        </div>
+
+                        <div className="flex flex-col items-center cursor-pointer bg-[#ffffff] py-4 rounded-l-2xl">
+                            <img src={settingsIcon} alt="settingsIcon" className="py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">SETTINGS</p>
+                        </div>
+
+                    </div>
+                    {/* Nav Items */}
+
+                    {/* Log Out  */}
+                    <div className=" w-[100%] relative flex flex-col justify-around h-[100%] ">
+                        <div  >
+                            <img src={curveRectangleDown} alt="curveRectangleDown" className="absolute top-0 right-0 "/>
+                        </div>
+
+                        <div className="flex flex-col items-center cursor-pointer ">
+                            <img src={logOut} alt="logOut" className=" py-2" />
+                            <p className="font-montserrat text-xs font-medium text-[#8F95A9]">LOGOUT</p>
+                        </div>
+                    </div>
+                    {/* Log Out  */}
+
+                </div>
+                {/* SideBar */}
+            </section>
+
+        </>
+    )
+}
